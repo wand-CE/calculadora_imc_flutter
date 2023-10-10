265b7a90d54bb26b5b1d4f2fa9490c0f6266f51b
wand-CE <wanderson14117@gmail.com>
2023-10-10 15:07:17 -0400

    changes on interface and functions

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+//Wanderson Soares dos Santos  -  UTF-8  -  09/10/2023  -  pt-Br
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter/rendering.dart';
 
 void main() {
+  debugPaintLayerBordersEnabled = false;
   runApp(const MyApp());
 }
 
@@ -11,6 +14,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.blueGrey,
+        textTheme: ThemeData.light().textTheme.apply(
+              bodyColor: Colors.white,
+            ),
+      ),
       home: ImcCalculator(),
       debugShowCheckedModeBanner: false,
     );
@@ -18,51 +27,110 @@ class MyApp extends StatelessWidget {
 }
 
 class ImcCalculator extends StatefulWidget {
+  const ImcCalculator({super.key});
+
   @override
-  _ImcCalculatorState createState() => _ImcCalculatorState();
+  ImcCalculatorState createState() => ImcCalculatorState();
 }
 
-class _ImcCalculatorState extends State<ImcCalculator> {
+TextFormField meuInput(controllerVariable, errorMessage, textOfLabel) {
+  TextFormField meuInput = TextFormField(
+    controller: controllerVariable,
+    validator: (value) {
+      if (value == null || value.isEmpty || value == ',' || value == '.') {
+        return errorMessage;
+      }
+    },
+    keyboardType: TextInputType.numberWithOptions(decimal: true),
+    inputFormatters: <TextInputFormatter>[
+      FilteringTextInputFormatter.allow(RegExp(r'^\d*[\.,]?\d*')),
+    ],
+    decoration: InputDecoration(
+      enabledBorder: OutlineInputBorder(
+        borderSide: BorderSide(
+          color: Colors.white,
+          width: 2.0,
+        ),
+      ),
+      labelText: textOfLabel,
+      labelStyle: TextStyle(
+        color: Colors.white,
+      ),
+      errorStyle: TextStyle(
+          fontSize: 20, color: const Color.fromARGB(255, 131, 26, 18)),
+      errorBorder: OutlineInputBorder(
+        borderSide: BorderSide(
+          color: const Color.fromARGB(255, 131, 26, 18),
+          width: 2.0,
+        ),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderSide: BorderSide(
+          color: Colors.blue,
+          width: 2.0,
+        ),
+      ),
+      focusedErrorBorder: OutlineInputBorder(
+        borderSide: BorderSide(
+          color: const Color.fromARGB(255, 131, 26, 18),
+          width: 2.0,
+        ),
+      ),
+    ),
+    style: TextStyle(
+      fontSize: 25,
+    ),
+  );
+
+  return meuInput;
+}
+
+class ImcCalculatorState extends State<ImcCalculator> {
   final _formKey = GlobalKey<FormState>();
-  final altura_input = TextEditingController();
-  final peso_input = TextEditingController();
-  Color result_color = Colors.transparent;
+  final alturaInput = TextEditingController();
+  final pesoInput = TextEditingController();
+  Color resultColor = Colors.transparent;
+  double? calculoIMC;
 
   String? result;
 
   String get classification {
-    String altura = altura_input.text.contains(',')
-        ? altura_input.text.replaceFirst(",", ".")
-        : altura_input.text;
+    String altura = alturaInput.text.contains(',')
+        ? alturaInput.text.replaceFirst(",", ".")
+        : alturaInput.text;
 
-    String peso = peso_input.text.contains(',')
-        ? peso_input.text.replaceFirst(",", ".")
-        : peso_input.text;
+    String peso = pesoInput.text.contains(',')
+        ? pesoInput.text.replaceFirst(",", ".")
+        : pesoInput.text;
 
-    double calculoIMC =
+    calculoIMC =
         (double.parse(peso) / (double.parse(altura) * double.parse(altura)));
 
-    if (calculoIMC < 18.5) {
+    calculoIMC = double.parse((calculoIMC!).toStringAsFixed(2));
+
+    if (calculoIMC! < 18.5) {
       return 'Baixo peso';
-    } else if (calculoIMC >= 18.5 && calculoIMC <= 24.99) {
+    } else if (calculoIMC! >= 18.5 && calculoIMC! <= 24.99) {
       return 'Peso normal';
-    } else if (calculoIMC >= 25 && calculoIMC <= 29.99) {
+    } else if (calculoIMC! >= 25 && calculoIMC! <= 29.99) {
       return 'Sobrepeso';
-    } else if (calculoIMC >= 30 && calculoIMC <= 34.99) {
+    } else if (calculoIMC! >= 30 && calculoIMC! <= 34.99) {
       return 'Obesidade grau I';
-    } else if (calculoIMC >= 35 && calculoIMC <= 39.99) {
+    } else if (calculoIMC! >= 35 && calculoIMC! <= 39.99) {
       return 'Obesidade grau II';
-    } else if (calculoIMC >= 40) {
+    } else if (calculoIMC! >= 40) {
       return 'Obesidade grau III';
     }
-    return 'oi';
+    return 'Erro ao calcular Imc';
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         title: Text('Calculadora IMC'),
+        backgroundColor: Colors.blue[200],
       ),
       body: Form(
         key: _formKey,
@@ -71,54 +139,20 @@ class _ImcCalculatorState extends State<ImcCalculator> {
             Center(
               child: Padding(
                 padding: const EdgeInsets.fromLTRB(16.0, 50.0, 16.0, 16.0),
-                child: TextFormField(
-                  controller: peso_input,
-                  validator: (value) {
-                    if (value == null ||
-                        value.isEmpty ||
-                        value == ',' ||
-                        value == '.') {
-                      return 'Por favor, insira seu peso';
-                    }
-                  },
-                  keyboardType: TextInputType.numberWithOptions(decimal: true),
-                  inputFormatters: <TextInputFormatter>[
-                    FilteringTextInputFormatter.allow(RegExp(r'^\d*[\.,]?\d*')),
-                  ],
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Digite seu peso em quilos',
-                  ),
-                  style: TextStyle(
-                    fontSize: 20,
-                  ),
+                child: meuInput(
+                  pesoInput,
+                  'Por favor, insira seu peso',
+                  'Digite seu peso em quilos',
                 ),
               ),
             ),
             Center(
               child: Padding(
                 padding: const EdgeInsets.fromLTRB(16.0, 20.0, 16.0, 30.0),
-                child: TextFormField(
-                  controller: altura_input,
-                  validator: (value) {
-                    if (value == null ||
-                        value.isEmpty ||
-                        value == ',' ||
-                        value == '.') {
-                      return 'Por favor, insira sua altura';
-                    }
-                  },
-                  keyboardType: TextInputType.numberWithOptions(decimal: true),
-                  inputFormatters: <TextInputFormatter>[
-                    FilteringTextInputFormatter.allow(RegExp(r'^\d*[\.,]?\d*')),
-                  ],
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Digite sua altura em metros',
-                  ),
-                  style: TextStyle(
-                    fontSize: 20,
-                  ),
+                child: meuInput(
+                  alturaInput,
+                  'Por favor, insira sua altura',
+                  'Digite sua altura em metros',
                 ),
               ),
             ),
@@ -138,19 +172,25 @@ class _ImcCalculatorState extends State<ImcCalculator> {
                     if (_formKey.currentState!.validate()) {
                       setState(() {
                         result = classification;
-                        result_color = Colors.black;
+                        resultColor = Colors.white;
                       });
                     }
                   },
                 ),
               ),
             ),
-            Center(
-              child: Text(
-                '$result',
-                style: TextStyle(fontSize: 30, color: result_color),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(10.0, 50.0, 10.0, 10.0),
+              child: Center(
+                child: Text(
+                  'Seu Imc é $calculoIMC\nVocê está com $result',
+                  style: TextStyle(
+                    fontSize: 23,
+                    color: resultColor,
+                  ),
+                ),
               ),
-            )
+            ),
           ],
         ),
       ),
